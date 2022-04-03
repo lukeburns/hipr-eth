f70005b8bcb891485f170cd13eebe20257e901f3
Luke Burns <lukemburns@gmail.com>
2022-04-03 16:10:44 -0700

    updates from buffrr/handover#nsec-ds

--- ethereum.js
+++ ethereum.js
@@ -1,55 +1,59 @@
+'use strict';
+
 const ethers = require('ethers');
-const { encoding, wire } = require('bns');
+const {encoding, wire, util} = require('bns');
 const LRU = require('blru');
 
 const ENS_ADDRESS = '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e';
 const ENS_ABI = [
-  'function setOwner(bytes32 node, address owner) external @500000',
-  'function setSubnodeOwner(bytes32 node, bytes32 label, address owner) external @500000',
-  'function setResolver(bytes32 node, address resolver) external @500000',
-  'function owner(bytes32 node) external view returns (address)',
-  'function resolver(bytes32 node) external view returns (address)'
+    'function setOwner(bytes32 node, address owner) external @500000',
+    'function setSubnodeOwner(bytes32 node, bytes32 label, address owner) external @500000',
+    'function setResolver(bytes32 node, address resolver) external @500000',
+    'function owner(bytes32 node) external view returns (address)',
+    'function resolver(bytes32 node) external view returns (address)'
 ];
 const RESOLVER_ABI = [
-  'function interfaceImplementer(bytes32 nodehash, bytes4 interfaceId) view returns (address)',
-  'function addr(bytes32 nodehash) view returns (address)',
-  'function setAddr(bytes32 nodehash, address addr) @500000',
-  'function name(bytes32 nodehash) view returns (string)',
-  'function setName(bytes32 nodehash, string name) @500000',
-  'function text(bytes32 nodehash, string key) view returns (string)',
-  'function setText(bytes32 nodehash, string key, string value) @500000',
-  'function contenthash(bytes32 nodehash) view returns (bytes)',
-  'function setContenthash(bytes32 nodehash, bytes contenthash) @500000',
-  'function ABI(bytes32 node, uint256 contentType) view returns (uint256, bytes)',
-  'function dnsRecord(bytes32 node, bytes32 name, uint16 resource) view returns (bytes)',
-  'function hasDNSRecords(bytes32 node, bytes32 name) view returns (bool)'
+    'function interfaceImplementer(bytes32 nodehash, bytes4 interfaceId) view returns (address)',
+    'function addr(bytes32 nodehash) view returns (address)',
+    'function setAddr(bytes32 nodehash, address addr) @500000',
+    'function name(bytes32 nodehash) view returns (string)',
+    'function setName(bytes32 nodehash, string name) @500000',
+    'function text(bytes32 nodehash, string key) view returns (string)',
+    'function setText(bytes32 nodehash, string key, string value) @500000',
+    'function contenthash(bytes32 nodehash) view returns (bytes)',
+    'function setContenthash(bytes32 nodehash, bytes contenthash) @500000',
+    'function ABI(bytes32 node, uint256 contentType) view returns (uint256, bytes)',
+    'function dnsRecord(bytes32 node, bytes32 name, uint16 resource) view returns (bytes)',
+    'function hasDNSRecords(bytes32 node, bytes32 name) view returns (bool)'
 ];
 
 const CACHE_TTL = 30 * 60 * 1000;
 const CACHE_TAGS = {
   DNS: 0,
   RESOLVER: 1
-};
+}
 
 class Ethereum {
-  constructor (options) {
+  constructor(options) {
     this.keccak256 = ethers.utils.keccak256;
     this.namehash = ethers.utils.namehash;
+
     this.provider = new ethers.getDefaultProvider('http://localhost:8545', options);
+
     this.ensRegistry = new ethers.Contract(ENS_ADDRESS, ENS_ABI, this.provider);
     this.ensResolver = null;
     this.cache = new EthereumCache(3000);
   }
 
-  async init () {
+  async init() {
     this.ensResolver = await this.getEnsResolver('eth');
   }
 
-  async getEnsResolver (name) {
+  async getEnsResolver(name) {
     return this.getResolver(name, ENS_ADDRESS);
   }
 
-  async getResolverFromRegistry (name, registry) {
+  async getResolverFromRegistry(name, registry) {
     const resolverAddr = await registry.resolver(this.namehash(name));
     if (resolverAddr === '0x0000000000000000000000000000000000000000') {
       return null;
@@ -58,7 +62,7 @@ class Ethereum {
     return new ethers.Contract(resolverAddr, RESOLVER_ABI, this.provider);
   }
 
-  async getResolver (name, registryAddress) {
+  async getResolver(name, registryAddress) {
     const cache = this.cache.getResolver(name, registryAddress);
     if (cache) {
       return cache;
@@ -74,35 +78,33 @@ class Ethereum {
     return resolver;
   }
 
-  getAbstractEnsRegistry (address) {
+  getAbstractEnsRegistry(address) {
     return new ethers.Contract(address, ENS_ABI, this.provider);
   }
 
-  async resolveEnsAddress (name) {
+  async resolveEnsAddress(name) {
     return this.provider.resolveName(name);
   }
 
   // https://eips.ethereum.org/EIPS/eip-634
-  async resolveEnsText (name, key) {
+  async resolveEnsText(name, key) {
     const nameResolver = await this.provider.getResolver(name);
 
-    if (!nameResolver) {
+    if (!nameResolver)
       return null;
-    }
 
     return nameResolver.getText(key);
   }
 
-  async resolveDnsFromEns (name, type, node) {
-    if (!node) {
+  async resolveDnsFromEns(name, type, node) {
+    if (!node)
       node = this.toNode(name);
-    }
 
-    const resolver = await this.getEnsResolver(this.trimDot(node));
+    const resolver = await this.getEnsResolver(util.trimFQDN(node));
     return this.resolveDnsWithResolver(name, type, node, resolver);
   }
 
-  async resolveDnsWithResolver (name, type, node, resolver) {
+  async getRRSet(name, type, node, resolver) {
     if (!resolver) {
       return null;
     }
@@ -110,7 +112,7 @@ class Ethereum {
     let record = this.cache.getRecord(name, type, resolver.address);
     if (!record) {
       record = await resolver.dnsRecord(
-        this.namehash(this.trimDot(node)),
+        this.namehash(util.trimFQDN(node)),
         this.hashDnsName(name),
         type
       );
@@ -118,96 +120,113 @@ class Ethereum {
       this.cache.setRecord(name, type, resolver.address, record);
     }
 
-    if (!record || record === '0x') {
-      if (type === wire.types.CNAME || type === wire.types.NS) {
-        return null;
-      }
-
-      // CNAME fallback:
-      // Technically, CNAME has priority over
-      // other record types in the zone
-      // but it adds a cost of an additional lookup
-      // to check for CNAME first due to
-      // how EIP-1185 works. For zones setup
-      // correctly this isn't an issue.
-      return this.resolveDnsWithResolver(
-        name,
-        wire.types.CNAME,
-        node,
-        resolver
-      );
-    }
+    if (!record || record === '0x')
+      return null;
 
     // prefixed with "0x" of course...
     return Buffer.from(record.substr(2), 'hex');
   }
 
-  async resolveDnsFromAbstractEns (name, type, addr, node) {
-    if (!node) {
-      node = this.toNode(name);
+  async resolveDnsWithResolver(name, type, node, resolver) {
+    const rrSet = await this.getRRSet(name, type, node, resolver);
+    if (rrSet) {
+      // if looking for NS, include DS in response
+      if (type === wire.types.NS) {
+        let dsSet = await this.getRRSet(name, wire.types.DS, node, resolver);
+        if (dsSet)
+          return Buffer.concat([rrSet, dsSet]);
+      }
+
+      return rrSet;
     }
 
-    if (addr.indexOf('.') >= 0 || addr.length !== 42) {
-      return null;
+    // NS/CNAME fallback:
+    // Technically, if NS/CNAME exists
+    // no other record types must be present
+    // but we don't have the full zone due to
+    // how EIP-1185 works. For zones setup
+    // correctly this isn't an issue.
+
+    // For NS lookups we use the node name not qname
+    // ideally we should keep adding labels (to the left)
+    // starting from node to qname until we find a delegation
+    // but this adds too many lookups
+    const sname = util.fqdn(node);
+    let nsSet = await this.getRRSet(sname, wire.types.NS, node, resolver);
+    if (nsSet) {
+      let dsSet = await this.getRRSet(sname, wire.types.DS, node, resolver);
+      if (dsSet)
+        return Buffer.concat([nsSet, dsSet]);
+
+      return nsSet;
     }
+
+    // Finally, look for a CNAME
+    return this.getRRSet(name, wire.types.CNAME, node, resolver);
+  }
+
+  async resolveDnsFromAbstractEns(name, type, ns, node) {
+    if (!node)
+      node = this.toNode(name);
+
+    const labels = ns.split('.');
+
+    if (labels.length !== 3)
+      return null;
+
+    if (labels[1] !== '_eth')
+      return null;
+
+    const addr = labels[0];
+    if (addr.length !== 42)
+      return null;
 
     const resolver = await this.getResolver(
-      this.trimDot(node),
+      util.trimFQDN(node),
       addr
     );
 
     return this.resolveDnsWithResolver(name, type, node, resolver);
   }
 
-  async resolveDnsFromRegistry (name, type, registryAddress, node) {
-    if (!node) {
+  async resolveDnsFromRegistry(name, type, registryAddress, node) {
+    if (!node)
       node = this.toNode(name);
-    }
 
     const resolver = await this.getResolver(
-      this.trimDot(node),
+      util.trimFQDN(node),
       registryAddress
     );
 
     return this.resolveDnsWithResolver(name, type, node, resolver);
   }
 
-  hashDnsName (name) {
+  hashDnsName(name) {
     const DNSName = encoding.packName(name);
     return this.keccak256(DNSName);
   }
 
-  trimDot (name) {
-    if (name[name.length - 1] === '.') {
-      return name.slice(0, -1);
-    }
+  toNode(name) {
+    let node = name
+    const labels = util.trimFQDN(name).split('.');
 
-    return name;
-  }
+    if (labels.length > 1)
+      node = labels.slice(-2).join('.')
 
-  toNode (name) {
-    let node = name;
-    const labels = this.trimDot(name).split('.');
-
-    if (labels.length > 1) {
-      node = labels.slice(-2).join('.');
-    }
-
-    return node;
+    return node
   }
 }
 
 class EthereumCache {
-  constructor (size) {
+  constructor(size) {
     this.cache = new LRU(size);
   }
 
-  setRecord (name, type, resolverAddress, record) {
+  setRecord(name, type, resolverAddress, record) {
     const key = this.toDnsKey(name, type, resolverAddress);
 
-    if (!record) {
-      record = '0x';
-    }
+    if (!record)
+      record = '0x'
 
     this.cache.set(key, {
       time: Date.now(),
@@ -215,25 +234,22 @@ class EthereumCache {
     });
   }
 
-  getRecord (name, type, resolverAddress) {
+  getRecord(name, type, resolverAddress) {
     const key = this.toDnsKey(name, type, resolverAddress);
     const item = this.cache.get(key);
 
-    if (!item) {
+    if (!item)
       return null;
-    }
 
-    if (Date.now() > item.time + CACHE_TTL) {
+    if (Date.now() > item.time + CACHE_TTL)
       return null;
-    }
 
     return item.record;
   }
 
-  setResolver (node, registryAddress, resolver) {
-    if (!resolver) {
+  setResolver(node, registryAddress, resolver) {
+    if (!resolver)
       return;
-    }
 
     const key = this.toResolverKey(node, registryAddress);
 
@@ -243,22 +259,20 @@ class EthereumCache {
     });
   }
 
-  getResolver (node, registryAddress) {
+  getResolver(node, registryAddress) {
     const key = this.toResolverKey(node, registryAddress);
     const item = this.cache.get(key);
 
-    if (!item) {
+    if (!item)
       return null;
-    }
 
-    if (Date.now() > item.time + CACHE_TTL) {
+    if (Date.now() > item.time + CACHE_TTL)
       return null;
-    }
 
     return item.resolver;
   }
 
-  toDnsKey (name, type, resolverAddress) {
+  toDnsKey(name, type, resolverAddress) {
     let key = CACHE_TAGS.DNS.toString() + ';';
     key += name + ';';
     key += type.toString() + ';';
@@ -267,7 +281,7 @@ class EthereumCache {
     return key;
   }
 
-  toResolverKey (node, registryAddress) {
+  toResolverKey(node, registryAddress) {
     let key = CACHE_TAGS.RESOLVER.toString() + ';';
     key += node + ';';
     key += registryAddress;
@@ -275,7 +289,7 @@ class EthereumCache {
     return key;
   }
 
-  reset () {
+  reset() {
     this.cache.reset();
   }
 }
--- index.js
+++ index.js
@@ -1,82 +1,88 @@
-const { util, wire, Zone } = require('bns')
-const { BufferReader } = require('bufio')
-const Ethereum = require('./ethereum')
-const { types } = wire
+const { util, wire, Zone } = require('bns');
+const { BufferReader } = require('bufio');
+const Ethereum = require('./ethereum');
+const { types, typesByVal } = wire;
 
-const empty = new Zone()
+const empty = new Zone();
 
-let ready = false
+let ready = false;
 const handleInit = () => {
-  ready = true
-  console.log('[eth] initialized')
-}
+  ready = true;
+  console.log('[eth] initialized');
+};
 const handleError = err => {
-  while (err.error) { err = err.error }
-  err = ((err.message || '').split(':').slice(-1)[0] || '').trim()
-  console.log(`[eth] failed to initialize : ${err} (retrying in 10s)`)
+  while (err.error) { err = err.error; }
+  err = ((err.message || '').split(':').slice(-1)[0] || '').trim();
+  console.log(`[eth] failed to initialize : ${err} (retrying in 10s)`);
 
   // retry every 10s
   setTimeout(() => {
-    ethereum.init().then(handleInit).catch(handleError)
-  }, 10000)
-}
-const ethereum = new Ethereum()
-ethereum.init().then(handleInit).catch(handleError)
+    ethereum.init().then(handleInit).catch(handleError);
+  }, 10000);
+};
+const ethereum = new Ethereum();
+ethereum.init().then(handleInit).catch(handleError);
 
-module.exports = middleware
+module.exports = middleware;
 
-function middleware () { 
+function middleware () {
+  // ethereum.resolveDnsFromAbstractEns('eskimo.forever.', types.A, `._eth.`)
   return {
-    hostname: ':data.:protocol(_eth|eth).:gateway?.', 
+    hostname: ':data.:protocol(_eth|eth).:gateway?.',
     handler: async function ({ protocol, data: hip5data }, name, type, res, rc, ns) {
-      type = types[type]
+      type = types[type];
 
       if (!ready) {
-        console.log('[eth] waiting for ethers to initialize')
+        console.log('[eth] waiting for ethers to initialize');
         const res = empty.resolve(name, types[type]);
         res.code = codes.SERVFAIL; // ensure response not cached
         return res;
       }
 
-      const labels = util.split(name)
+      let data;
       if (protocol === 'eth') {
-        const data = await ethereum.resolveDnsFromEns(name, type)
-        if (data && data.length > 0) {
-          return sendData(data, type)
-        }
-      }
-
-      if (protocol === 'eth') {
-        data = await ethereum.resolveDnsFromEns(name, type, hip5data+'.eth')
+        data = await ethereum.resolveDnsFromEns(name, type, `${hip5data}.eth.`);
       } else if (protocol === '_eth') {
-        data = await ethereum.resolveDnsFromAbstractEns(name, type, hip5data)
+        data = await ethereum.resolveDnsFromAbstractEns(name, type, `${hip5data}._eth.`);
+        console.log('[eth]', name, typesByVal[type], `${hip5data}._eth.\n`, sendData(data).toString());
       }
 
       if (!data || data.length === 0) {
-        return empty.resolve(name, type)
+        return empty.resolve(name, type);
       }
 
-      return sendData(data, type)
+      return sendData(data, type);
     }
-  }
+  };
 }
 
 function sendData (data, type) {
-  const res = new wire.Message()
-  res.aa = true
-  const br = new BufferReader(data)
+  const res = new wire.Message();
+  // res.aa = true;
+  const br = new BufferReader(data);
   while (br.left() > 0) {
-    res.answer.push(wire.Record.read(br))
+    const rr = wire.Record.read(br);
+    if (rr.type === wire.types.NS) {
+      res.authority.push(rr);
+    } else if (rr.type === type || rr.type === wire.types.CNAME) {
+      res.answer.push(rr);
+    } else {
+      res.authority.push(rr);
+    }
   }
 
-  // todo: sign properly
-  // // Answers resolved from alternate name systems appear to come directly
-  // // from the HNS root zone.
-  // this.ns.signRRSet(res.answer, type)
-
-  // if (type !== wire.types.CNAME) {
-  //   this.ns.signRRSet(res.answer, wire.types.CNAME)
+  // Referral answer
+  // if (res.answer.length === 0 && res.authority.length > 0) {
+  //   res.aa = false;
+  //   // this.ns.signRRSet(res.authority, wire.types.DS);
   // }
 
-  return res
+  // Answers resolved from alternate name systems appear to come directly
+  // from the HNS root zone.
+  // this.ns.signRRSet(res.answer, type);
+
+  // if (type !== wire.types.CNAME)
+  // this.ns.signRRSet(res.answer, wire.types.CNAME);
+
+  return res;
 }
